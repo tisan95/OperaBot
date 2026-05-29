037429eed5ac4062142919b1170b0fa3e1f1f260
Santiago Castello <santiagoxtv@gmail.com>
2026-05-29 17:03:46 +0000

    fix: proxy Next.js para resolver cross-origin de cookies

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     esmExternals: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
